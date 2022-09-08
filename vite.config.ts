58f045a36263e1e8d3644296391f359c82d6f5e8
Asım Tahir <AsimTahir@protonmail.com>
2022-09-08 09:52:11 +0300

    chore(deps): :heavy_plus_sign: add `unplugin-vue-components` vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 
 import Vue from "@vitejs/plugin-vue";
 import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,6 +17,10 @@ export default defineConfig({
       ],
       dts: "types/generated/auto-imports.d.ts",
       imports: ["vue"],
+    }),
+    Components({
+      dirs: ["src/components"],
+      dts: "types/generated/components.d.ts",
     }),
   ],
   resolve: {
